8e41ab9d9a9160fdc383482a683ad8f81bd6ae70
Yusufcan Yilmaz <playders03@gmail.com>
2026-03-03 13:01:57 +0300

    feat: setup axios for future-proofing

---
+++ src/lib/axios.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError } from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
